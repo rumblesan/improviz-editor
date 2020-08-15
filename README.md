f433cd6fc8887e65c28396029d1f8a71e6abd73a
Guy John <guy@rumblesan.com>
2020-08-15 20:55:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Improviz Editor
+
+A basic editor for use with Improviz.
+
+Very much a work in progress.
+
+
+## Contact
+
+Drop me an email at guy@rumblesan.com
+
+
+## License
+
+BSD License.
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./stylesheets/main.scss";
+
+// Small helpers you might want to keep
+import "./helpers/context_menu.js";
+import "./helpers/external_links.js";
+
+// ----------------------------------------------------------------------------
+// Everything below is just to show you how it works. You can delete all of it.
+// ----------------------------------------------------------------------------
+
+import { greet } from "./hello_world/hello_world";
+import env from "env";
